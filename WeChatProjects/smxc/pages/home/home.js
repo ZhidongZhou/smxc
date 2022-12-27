2858cef49c600b0875b1ecb912cadeaf4c6909c2
ZhidongZhou <zdzorg@163.com>
2022-12-27 17:06:45 +0800

    前端-首页产品列表实现

--- WeChatProjects/smxc/pages/home/home.js
+++ WeChatProjects/smxc/pages/home/home.js
@@ -15,14 +15,44 @@ Page({
     vertical: false,
     autoplay: true,
     interval: 2000,
-    duration: 500
+    duration: 500,
+    productData: [] // Todo: 请求接口的域名需要在: 微信小程序 -> 配置服务器信息. 中修改
   },
 
   /**
    * Lifecycle function--Called when page load
    */
   onLoad(options) {
-
+    // 主页面加载时调用获取产品列表数据的函数
+    this.getProductData();
+  },
+  
+  /**
+   * 产品列表数据请求函数
+   */
+  getProductData:function(){
+    let self = this // 解决 setData 的 this 指向性问题
+    wx.request({
+      url: 'http://127.0.0.1:3006/products?pageNum=1&pageSize=10',
+      data: {
+        pageNum: 1,
+        pageSize: 10,
+      },
+      header: {
+        'content-type': 'application/json' // 默认值
+      },
+      success(res) {
+        console.log("total products: ",res.data.result.list.length)
+        let productList = res.data
+        // 判断获取成功， 赋值给 Data -> productData
+        if(productList.code == 0 && productList.result.list.length>0) {
+          // 注意 this 指向性问题 需要 let self = this
+          self.setData({
+            productData: productList.result.list
+          })
+        }
+      }
+    })
   },
 
   /**
